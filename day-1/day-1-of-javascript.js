b2fa7618e739d75000507bdd2d88a2bd0c6ee841
Aakash Pawar <aakashpawar1999@gmail.com>
2021-06-10 08:01:06 +0530

    Added day 1 files

---
+++ day-1/day-1-of-javascript.js
@@ -0,0 +1,31 @@
+// console.log("Hello World!");
+
+// console.info("Aakash");
+
+// console.groupCollapsed("Group");
+// console.warn("This is a warning");
+// console.groupEnd();
+
+// console.error("This is an error");
+
+// console.clear();
+
+// console.assert(true, "This is false");
+
+// for (i = 0; i < 10; i++) {
+//     console.count("Iteration");
+// }
+
+// console.time();
+// for (i = 0; i < 1000000000; i++) {
+//     // statement
+// }
+// console.timeEnd();
+
+function oldFunction() {
+    newFunction();
+}
+
+function newFunction() {
+    console.trace();
+}
