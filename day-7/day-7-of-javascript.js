0c56997a38de741b90b51852478003476e84a273
Aakash Pawar <aakashpawar1999@gmail.com>
2021-09-05 10:41:58 +0530

    Added day 7 files

---
+++ day-7/day-7-of-javascript.js
@@ -0,0 +1,21 @@
+let a = 50;
+let b = 50;
+
+// if..else statement
+if (a == b) {
+    console.log("a and b are equal");
+} else {
+    console.log("a and b are not equal");
+}
+
+// else..if statement
+if (a == 5) {
+    console.log("a is equal to 5");
+} else if (a == 10) {
+    console.log("a is equal to 10");
+} else {
+    console.log("wrong value");
+}
+
+// ternary operator
+console.log((a === b) ? "a is equal to b" : "a is not equal to b");
