16e28698d35c2d5af43df44ac2d4de184229a0c1
Aakash Pawar <aakashpawar1999@gmail.com>
2021-11-21 09:55:20 +0530

    Added day 10 files

---
+++ day-10/day-10-of-javascript.js
@@ -0,0 +1,16 @@
+let x = 1;
+
+// Strict comparison (Value and Type should match with Case)
+// let x = "1";  // here, the type is of string
+
+switch (x) {
+	default:
+		console.log("wrong choice");
+		break;
+	case 1:
+		console.log("Case 1");
+		break;
+	case 2:
+		console.log("Case 2");
+		break;
+}
